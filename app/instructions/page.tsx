d4298e737e10cad3448550b94b2ea26e908b3303
Harindu Lakshan <harindulakshitha525@gmail.com>
2025-05-25 12:56:35 +0530

    Remove InstructionsPage component from instructions directory

--- app/instructions/page.tsx
+++
@@ -1,9 +0,0 @@
-import Instructions from '@/components/Instructions'
-
-export default function InstructionsPage() {
-  return (
-    <main className="min-h-screen bg-background">
-      <Instructions />
-    </main>
-  )
-} 
